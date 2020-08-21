01071a4a6a153b2347a08555cc1fc4c47c17fddd
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-08-21 12:14:14 -0300

    Request for endpoint.

---
+++ app.js
@@ -0,0 +1,8 @@
+let page = 1
+
+const getPosts = async () => {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
+    const data = await response.json()
+
+    return data
+}
