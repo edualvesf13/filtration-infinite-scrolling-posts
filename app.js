c82a19cbf7e115b5bd18682852c14f99a258085d
Eduardo ALves Ferreira <edualvesf13@gmail.com>
2020-08-21 12:37:23 -0300

    Insert posts in to DOM.

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
+const postsContainer = document.querySelector('#posts-container')
 let page = 1
 
 const getPosts = async () => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
-    const data = await response.json()
-
-    return data
+    return response.json()
 }
+
+const addPostInToDOM = async () => {
+    const posts = await getPosts()
+    const postsTemplate = posts.map( ({id, title, body}) => `
+        <div class="post">
+            <div class="number"> ${id} </div>
+            <div class="post-info"> 
+                <h2 class="post-title"> ${title} </h2>
+                <p class="post-body"> ${body} </p>
+            </div>
+        </div>
+    ` ).join('')
+
+    postsContainer.innerHTML += postsTemplate
+}
+
+addPostInToDOM()
